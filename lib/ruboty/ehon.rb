5f80bced0f69b8c7d5d02a37ac80671b7da0329f
Risa Harada <supikateriburu@gmail.com>
2017-10-25 01:57:42 +0900

    :seedling: Initial commit

---
+++ lib/ruboty/ehon.rb
@@ -0,0 +1,7 @@
+require "ruboty/ehon/version"
+
+module Ruboty
+  module Ehon
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/ehon/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Ehon
+    VERSION = "0.1.0"
+  end
+end
