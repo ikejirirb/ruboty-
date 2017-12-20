ce7a1e6e2e576c20efef2f12edcfa1777734dbb5
Risa Harada <supikateriburu@gmail.com>
2017-12-20 20:57:57 +0900

    :sparkles: 同じタイトルの絵本は追加しない

--- spec/ruboty/ehon_spec.rb
+++ spec/ruboty/ehon_spec.rb
@@ -11,12 +11,34 @@ describe Ruboty::Handlers::Ehon do
     Ruboty::Robot.new
   end
 
+  ehon = "わたしのワンピース"
+
   describe "ping" do
     it "replies to message \"ping\" and gives you \"pong\"" do
       expect(robot).to receive(:say).with(
         hash_including(body: "pong")
       )
       robot.receive(body: "#{robot.name} ping")
+    end
+  end
+
+  describe "絵本追加" do
+    it "replies to message \"絵本追加 #{ehon}\"" do
+      expect(robot).to receive(:say).with(
+        hash_including(body: "「#{ehon}」を追加したよ！")
+      )
+      robot.receive(body: "#{robot.name} 絵本追加 #{ehon}")
+    end
+
+    it "おなじタイトルの絵本は追加できない" do
+      expect(robot).to receive(:say).with(
+        hash_including(body: "「#{ehon}」を追加したよ！")
+      )
+      robot.receive(body: "#{robot.name} 絵本追加 #{ehon}")
+      expect(robot).to receive(:say).with(
+        hash_including(body: "「#{ehon}」はもうあるよ")
+      )
+      robot.receive(body: "#{robot.name} 絵本追加 #{ehon}")
     end
   end
 end
