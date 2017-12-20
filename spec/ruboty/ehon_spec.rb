e23518bd57172ff2b0eba907347265c6696f8abc
Risa Harada <supikateriburu@gmail.com>
2017-12-20 20:30:30 +0900

    :green_heart: Add test

--- spec/ruboty/ehon_spec.rb
+++ spec/ruboty/ehon_spec.rb
@@ -5,3 +5,18 @@ RSpec.describe Ruboty::Ehon do
     expect(Ruboty::Ehon::VERSION).not_to be nil
   end
 end
+
+describe Ruboty::Handlers::Ehon do
+  let(:robot) do
+    Ruboty::Robot.new
+  end
+
+  describe "ping" do
+    it "replies to message \"ping\" and gives you \"pong\"" do
+      expect(robot).to receive(:say).with(
+        hash_including(body: "pong")
+      )
+      robot.receive(body: "#{robot.name} ping")
+    end
+  end
+end
